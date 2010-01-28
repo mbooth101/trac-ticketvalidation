982941237a34167fc21af232627dbdd0d4404a86
Mat Booth <mbooth@apache.org>
2010-01-28 19:59:29 +0000

    Can now edit and save rules that already exist through the admin panel.

--- ticketvalidation/admin.py
+++ ticketvalidation/admin.py
@@ -4,31 +4,25 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
-from trac.core import implements, Component
-from trac.admin.api import IAdminPanelProvider
+from trac.ticket.admin import TicketAdminPanel
 from trac.ticket.api import TicketSystem
 from trac.util.translation import _
+from trac.admin.web_ui import _save_config
 
 from ticketvalidation.main import TicketValidationRules
 
 __all__ = ['TicketValidationAdminPanel']
 
 
-class TicketValidationAdminPanel(Component):
+class TicketValidationAdminPanel(TicketAdminPanel):
     """Provides an admin panel for configuring ticket validation rules."""
 
-    implements(IAdminPanelProvider)
-
+    _type = 'validation'
     _label = (_('Validation Rule'), _('Validation Rules'))
 
     # IAdminPanelProvider methods
 
-    def get_admin_panels(self, req):
-        if 'TICKET_ADMIN' in req.perm:
-            yield ('ticket', 'Ticket System', 'validation', self._label[1])
-
-    def render_admin_panel(self, req, cat, page, path_info):
-        req.perm.require('TICKET_ADMIN')
+    def _render_admin_panel(self, req, cat, page, path_info):
         rules = TicketValidationRules(self.env).get_rules()
         rule = [r for r in rules if r['name'] == path_info]
 
@@ -36,6 +30,25 @@ class TicketValidationAdminPanel(Component):
         if path_info and rule:
             if req.method == 'POST':
                 if req.args.get('save'):
+                    name = str(req.args.get('name')).strip()
+                    condition = str(req.args.get('condition')).strip()
+                    assert name, 'Cannot save a rule with no name'
+                    assert condition, 'Cannot save a rule with no condition'
+                    self.config.remove('ticket-validation', rule[0]['name'])
+                    self.config.set('ticket-validation', name, condition)
+
+                    for opt in ('required', 'hidden'):
+                        value = req.args.get(opt)
+                        self.config.remove('ticket-validation',
+                                           '%s.%s' % (rule[0]['name'], opt))
+                        if isinstance(value, list):
+                            self.config.set('ticket-validation',
+                                            '%s.%s' % (name, opt), " ".join(value))
+                        else:
+                            self.config.set('ticket-validation',
+                                            '%s.%s' % (name, opt), value)
+
+                    _save_config(self.config, req, self.log)
                     req.redirect(req.href.admin(cat, page))
                 elif req.args.get('cancel'):
                     req.redirect(req.href.admin(cat, page))
